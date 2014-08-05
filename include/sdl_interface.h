22f6c9dffa5e9046f0fa263316620f05f4cd7d09
SergNikitin <snikitin@outlook.com>
2014-08-05 13:22:59 +0400

    added method to get bitmap from image file

---
+++ include/sdl_interface.h
@@ -0,0 +1,9 @@
+#ifndef __SDL_INTERFACE_H__
+#define __SDL_INTERFACE_H__
+
+#include <string>
+#include "grayscale_bitmap.h"
+
+FramedBitmap loadGrayscaleImage(const std::string& filepath);
+
+#endif // __SDL_INTERFACE_H__
--- src/sdl_interface.cpp
+++ src/sdl_interface.cpp
@@ -1,1 +1,25 @@
+#include "SDL.h"
+#include "SDL_image.h"
+
 #include "sdl_interface.h"
+
+class SdlMaintainer {
+public:
+    SdlMaintainer() : surface(nullptr){};
+
+    virtual ~SdlMaintainer() {
+        SDL_FreeSurface(surface);
+    }
+
+    SDL_Surface* surface;
+
+private:
+    SdlMaintainer(const SdlMaintainer&);
+};
+
+static SdlMaintainer maintainer;
+
+FramedBitmap loadGrayscaleImage(const std::string& filepath) {
+    maintainer.surface = IMG_Load(filepath.c_str());
+    return FramedBitmap(maintainer.surface);
+}
